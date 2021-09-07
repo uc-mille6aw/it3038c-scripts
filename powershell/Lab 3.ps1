a2443ad32f24f10cc69ad1ceeb7ec497b7652424
uc-mille6aw <mille6aw@mail.uc.edu>
2021-09-07 15:13:26 -0400

    powershell files

---
+++ powershell/Lab 3.ps1
@@ -0,0 +1,26 @@
+﻿function getIP{
+(Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+
+function getAccount{
+($env:username)
+}
+
+function getHOSTNAME{
+($env:COMPUTERNAME)
+}
+
+function getVersion{
+($Host.version.major)
+}
+
+function getDate {
+(Get-Date -format "dddd, MM-dd-yyyy")
+}
+
+$IP=getIP
+$User=getAccount
+$HostName=getHOSTNAME
+$Version=getVersion
+$Date=getDate
+Write-Output("This machine's IP is $IP. User is $User. Hostname is $HostName. Powershell Version $Version. Today's date is $Date.") >> C:\it3038c-scripts\powershell\Lab3.txt
