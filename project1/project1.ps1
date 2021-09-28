31c6b87138b3cbec7f7abf63534796acca143fe5
uc-mille6aw <mille6aw@mail.uc.edu>
2021-09-28 16:31:50 -0400

    Updated some files

--- project1/project1.ps1
+++ project1/project1.ps1
@@ -1,11 +1,15 @@
-﻿#Ask the user for the drive letter of the driver we will be searching. 
-$userInput = Read-Host -Prompt 'Enter the drive letter for tyhe drive you would like to search'
+﻿#Ask the user for the drive letter of the drive we will be searching. 
+$userInput = Read-Host -Prompt 'Enter the drive letter for the drive you would like to search'
 
-# this command searches the drive the user specified for .mp4 files that re larger than 50000000 bytes, or 50 MB. The output is displayed as a table and then sent out to a text file in the same directory that the script is in.
+#These 2 lines save the current working directory location of the scripts to the variable $dir. Without this, the out-file output will not go where the user can easily find it.
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
 
-Get-ChildItem ${userInput}:\ -recurse -include *.mp4 -EA SilentlyContinue | where-object {$_.length -gt 50000000} | Sort-Object fullname | format-table fullname, length, LastAccessTime -auto | out-file .\large_videos.txt
+# this command searches the drive the user specified for .mp4 files that are larger than 50000000 bytes, or 50 MB. The output is displayed as a table and then converted to a string (for formating reasons) Lastly, the output is sent to a text file in the same directory that the script is in.
 
-#this pause is for troubleshootin and to let the user know when the command has finsished searching. 
+Get-ChildItem ${userInput}:\ -recurse -include *.mp4 -EA SilentlyContinue | where-object {$_.length -gt 50000000} | Sort-Object fullname | format-table fullname, length, LastAccessTime -AutoSize | out-string -width 10000 | out-file $dir\large_videos.txt
+
+#this pause is for troubleshooting and to let the user know when the command has finished searching. 
 
 PAUSE
 
