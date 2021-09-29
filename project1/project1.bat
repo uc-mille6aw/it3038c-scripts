d795ceabb1cd2cbdb3ed2ef58656959ab3c61abd
uc-mille6aw <mille6aw@mail.uc.edu>
2021-09-29 13:49:22 -0400

    added comments to batch file/script launcher

--- project1/project1.bat
+++ project1/project1.bat
@@ -1,3 +1,5 @@
 @ECHO OFF
+REM This command launches powershell, bypasses the execution policy setting that prevents scripts from running, and runs powershell as an admin. 
+REM It uses the special function %~dpn0.ps1 to find ps1 files in the same direcotry as the .bat file.
 PowerShell.exe -Command "& {Start-Process PowerShell.exe '-ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAS}"
 PAUSE
