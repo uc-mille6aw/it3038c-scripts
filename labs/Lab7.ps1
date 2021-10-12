938e72ff19d812c4c3661d2b2606981a6ca9795f
uc-mille6aw <mille6aw@mail.uc.edu>
2021-10-12 14:17:20 -0400

    Lab 7 files

---
+++ labs/Lab7.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module NTFSSecurity
+
+$userPath = Read-Host -prompt "Enter the path where you would like to learn more about security settings"
+
+cd $userPath
+
+Add-Content -path .\NtfsInfo.txt -value "An overview of all permissions:" 
+
+dir | Get-NTFSAccess | format-table -autosize | out-file -append -encoding ascii -FilePath .\NtfsInfo.txt
+
+Add-Content -path .\NtfsInfo.txt -value "A list of owners:"
+
+dir | Get-NTFSOwner | out-file -append -encoding ascii -FilePath .\NtfsInfo.txt
+
+Add-Content -path .\NtfsInfo.txt -value "File permission inheritance information:"
+
+dir | Get-NTFSInheritance | out-file -append -encoding ascii -FilePath .\NtfsInfo.txt
